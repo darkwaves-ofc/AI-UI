3a5e50ceb3f07617ed56b8e5dbd6f4e312608c93
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2024-02-20 21:07:34 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,13 +7,6 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# login to the vps without password prompt
-sudo -n true
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Passwordless sudo is not configured. Please configure passwordless sudo for the current user.${NC}"
-    exit 1
-fi
-
 # Function to display progress using whiptail
 show_progress() {
     {
@@ -27,12 +20,17 @@ show_progress() {
         echo "80"
         sleep 1
         echo "100"
-    } | whiptail --gauge "${BLUE}Updating and Building${NC}" 6 50 0
+    } | whiptail --gauge "${BLUE}Updating and Building${NC}"  50 06
 }
 
 # Function to display a message box
 show_message() {
     whiptail --title "${GREEN}Message${NC}" --msgbox "$1" 8 50
+}
+
+# Function to display an error message
+show_error() {
+    echo -e "${RED}$1${NC}"
 }
 
 # Main function
@@ -44,18 +42,43 @@ main() {
     echo -e "${BLUE}Wellcome Developer! Starting Ollama on DarkWaves${NC}"
     echo -e "${BLUE}Updating and building...${NC}"
     git stash &>/dev/null
+    if [ $? -ne 0 ]; then
+        show_error "Error: Unable to stash changes."
+        exit 1
+    fi
     echo -e "${BLUE}Stashing changes...${NC}"
+    
     git pull &>/dev/null
+    if [ $? -ne 0 ]; then
+        show_error "Error: Unable to pull latest changes."
+        exit 1
+    fi
     echo -e "${BLUE}Pulling latest changes...${NC}"
+    
     npm install --force &>/dev/null
+    if [ $? -ne 0 ]; then
+        show_error "Error: Unable to install npm packages."
+        exit 1
+    fi
     echo -e "${BLUE}Installing npm packages...${NC}"
+    
     echo -e "${BLUE}Building project... and starting server...${NC}"
     npm run build | while IFS= read -r line; do
         echo -e "${YELLOW}${line}${NC}"
     done
+    if [ $? -ne 0 ]; then
+        show_error "Error: Build failed."
+        exit 1
+    fi
+    
     npm run start | while IFS= read -r line; do
         echo -e "${YELLOW}${line}${NC}"
     done
+    if [ $? -ne 0 ]; then
+        show_error "Error: Failed to start server."
+        exit 1
+    fi
+    
     echo -e "${GREEN}Update and build process complete.${NC}"
     
     # Kill progress dialog
